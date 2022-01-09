76a343eb46dbe4aa63b9a5bdb5b2c95d5f554c06
Abhishek-N <abhisheknshetty97@gmail.com>
2022-01-09 22:30:34 +0530

    create rooms

--- base/views.py
+++ base/views.py
@@ -1,11 +1,18 @@
+from typing import ContextManager
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {'id': 1, 'name': 'Lets learn python'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Front End Developers'}
+]
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    return render(request, 'base/room.html')

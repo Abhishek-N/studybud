e4811f3b67e5d34d311578de4aea036a191ef126
Abhishek-N <abhisheknshetty97@gmail.com>
2022-01-17 23:18:44 +0530

    add rooms

--- base/views.py
+++ base/views.py
@@ -1,24 +1,22 @@
-from typing import ContextManager
-from django.http.response import HttpResponse
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': 'Lets learn python'},
-    {'id': 2, 'name': 'Design with me'},
-    {'id': 3, 'name': 'Front End Developers'}
-]
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn python'},
+#     {'id': 2, 'name': 'Design with me'},
+#     {'id': 3, 'name': 'Front End Developers'}
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
+    
     context = {'room': room}
     return render(request, 'base/room.html', context)
 
